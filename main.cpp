5dec54f2c4de88d5ad2e8efee95f578951c63ca9
Veirisa <anya-rodionova_all@mail.ru>
2018-02-27 23:04:00 +0300

    deviation fixed + big tests added

--- main.cpp
+++ main.cpp
@@ -20,62 +20,47 @@ mt19937 generator(time(0));
 size_t calc[100];
 
 template <typename OPL>
-void testing(const string& algorithm_name) {
+void testing(ofstream& fout, size_t lambda, size_t n) {
+    OPL gen(lambda, n);
+    size_t calculatings = 0, deviation = 0;
+    for (size_t test = 0; test < 100; ++test) {
+        calc[test] = gen.generate_solution(random_string(generator, n));
+        calculatings += calc[test];
+    }
+    calculatings /= 100;
+    for (size_t i = 0; i < 100; ++i) {
+        deviation += (calc[i] - calculatings) * (calc[i] - calculatings);
+    }
+    deviation = (size_t)sqrt(deviation / 100);
+    cout << "lambda: " << lambda << "  n: " << n << "\n";
+    fout << "lambda: " << lambda << "  n: " << n << "  calculatings: " << calculatings << "  deviation: " << deviation << "\n";
+}
+
+template <typename OPL>
+void full_testing(const string& algorithm_name, bool withHard) {
     cout <<  "\n" << algorithm_name << "\n\n";
     ofstream fout(algorithm_name + ".txt");
     fout << "testing " << algorithm_name << ":\n\n";
-
     for (size_t lambda = 1; lambda <= 10; ++lambda) {
         for (size_t n = 100; n <= 1000; n += 100) {
-            OPL gen(lambda, n);
-            size_t calculatings = 0, deviation = 0;
-            for (size_t test = 0; test < 100; ++test) {
-                calc[test] = gen.generate_solution(random_string(generator, n));
-                calculatings += calc[test];
-            }
-            calculatings /= 100;
-            for (size_t i = 0; i < 100; ++i) {
-                deviation += (calc[i] - calculatings) * (calc[i] - calculatings);
-            }
-            deviation = (size_t)sqrt(deviation / n);
-            cout << "lambda: " << lambda << "  n: " << n << "\n";
-            fout << "lambda: " << lambda << "  n: " << n << "  calculatings: " << calculatings << "  deviation: " << deviation << "\n";
+            testing<OPL>(fout, lambda, n);
         }
         fout << "\n";
     }
-}
-
-size_t BIG_LAMBDA = 200;
-
-template <typename OPL>
-void hard_testing(const string& algorithm_name) {
-    cout <<  "\n" << algorithm_name + " (hard)" << "\n\n";
-    ofstream fout(algorithm_name + "_(big).txt");
-    fout << "hard testing " << algorithm_name << ":\n\n";
-    for (size_t n = 10000; n <= 100000; n += 10000) {
-        OPL gen(BIG_LAMBDA, n);
-        size_t calculatings = 0, deviation = 0;
-        for (size_t test = 0; test < 100; ++test) {
-            calc[test] = gen.generate_solution(random_string(generator, n));
-            calculatings += calc[test];
+    if (withHard) {
+        for (size_t lambda = 50; lambda <= 800; lambda *= 2) {
+            for (size_t n = 10000; n <= 100000; n += 10000) {
+                testing<OPL>(fout, lambda, n);
+            }
+            fout << "\n";
         }
-        calculatings /= 100;
-        for (size_t i = 0; i < 100; ++i) {
-            deviation += (calc[i] - calculatings) * (calc[i] - calculatings);
-        }
-        deviation = (size_t)sqrt(deviation / n);
-        cout << "lambda: " << BIG_LAMBDA << "  n: " << n << "\n";
-        fout << "lambda: " << BIG_LAMBDA << "  n: " << n << "  calculatings: " << calculatings << "  deviation: " << deviation << "\n";
     }
-
 }
 
 int main() {
-    //testing<one_plus_lambda>("one_plus_lambda");
-    //testing<one_plus_lambda_fast>("one_plus_lambda_fast");
-    hard_testing<one_plus_lambda_fast>("one_plus_lambda_fast");
-    //testing<one_plus_lambda_tuning>("one_plus_lambda_tuning");
-    //testing<one_plus_lambda_tuning_fast>("one_plus_lambda_tuning_fast");
-    hard_testing<one_plus_lambda_tuning_fast>("one_plus_lambda_tuning_fast");
+    full_testing<one_plus_lambda>("one_plus_lambda", false);
+    full_testing<one_plus_lambda_fast>("one_plus_lambda_fast", true);
+    full_testing<one_plus_lambda_tuning>("one_plus_lambda_tuning", false);
+    full_testing<one_plus_lambda_tuning_fast>("one_plus_lambda_tuning_fast", true);
     return 0;
 }
